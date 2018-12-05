aec387490283fd1fff272b20840e7eb3b3dd7b6b
kairis <kairis@aliyun.com>
2018-12-05 21:01:06 +0800

    kairis

---
+++ kairis/src/main/scala/kairis.scala
@@ -0,0 +1,10 @@
+object kairis {
+
+  def main(args: Array[String]): Unit = {
+    println("kkkkkk")
+    println("kkkkkk")
+    println("kkkkkk")
+    println("kkkkkk")
+    println("kkkkkk")
+  }
+}
